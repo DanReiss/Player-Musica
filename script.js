ba90f2d4ddc137fe6df451f26f6c9a5f6e5381a0
Danreiss <danreis.ctt@gmail.com>
2023-03-06 21:20:19 -0300

    melhorias
    Adição de controle de volume, função de manipular o andamento da música, pequenas mudanças no design

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ let duracaoMusica = document.querySelector('.end');
 let imagem = document.querySelector('img');
 let musicName = document.querySelector('.title-name h2');
 let artistsName = document.querySelector('.title-name p');
+let muteButton = document.querySelector('.mute-button')
+let volumeInput = document.querySelector('.volume-input')
+let progressInput = document.querySelector('.progress-input');
 
 renderMusic(indexMusic);
 
@@ -18,24 +21,15 @@ document.querySelector('.botaoplay').addEventListener('click', playMusic);
 
 document.querySelector('.botaopause').addEventListener('click', stopMusic);
 
-musica.addEventListener('timeupdate', updateBar);
+muteButton.addEventListener('click', muteMusic);
 
-document.querySelector('.back').addEventListener('click', () => {
-    indexMusic--;
-    if(indexMusic < 0 ){
-        indexMusic = 4;
-    }
-    renderMusic(indexMusic);
-    playMusic();
-});
-document.querySelector('.next').addEventListener('click', () => {
-    indexMusic++;
-    if(indexMusic > 4){
-        indexMusic = 0;
-    }
-    renderMusic(indexMusic);
-    playMusic();
-});
+volumeInput.addEventListener('change', changeVolume)
+
+progressInput.addEventListener('change', changeCurrentTime)
+ 
+document.querySelector('.back').addEventListener('click', back);
+document.querySelector('.next').addEventListener('click', next);
+
 
 function renderMusic(index){
     musica.setAttribute('src', musics[index].src);
@@ -44,24 +38,75 @@ function renderMusic(index){
         artistsName.textContent = musics[index].artists;
         imagem.src = musics[index].img;
         duracaoMusica.textContent = secToMin(Math.floor(musica.duration));
+        progressInput.max = Math.floor(musica.duration);
+        changeVolume();
     });
 }
+
 function playMusic(){
     musica.play();
     document.querySelector('.botaopause').style.display = 'block';
     document.querySelector('.botaoplay').style.display = 'none';
 }
+
 function stopMusic(){
     musica.pause();
     document.querySelector('.botaopause').style.display = 'none';
     document.querySelector('.botaoplay').style.display = 'block';
 }
-function updateBar(){
-    let barra = document.querySelector('progress');
-    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+
+function back(){
+    indexMusic--;
+    if(indexMusic < 0 ){
+        indexMusic = 4;
+    }
+    renderMusic(indexMusic);
+    playMusic();
+}
+
+function next(){
+    indexMusic++;
+    if(indexMusic > 4){
+        indexMusic = 0;
+    }
+    renderMusic(indexMusic);
+    playMusic();
+}
+
+function muteMusic(){
+    let iconClass = musica.muted ? "fa-solid fa-volume-high" : "fa-solid fa-volume-xmark"
+    musica.muted = !musica.muted
+    
+    muteButton.children[0].classList = iconClass
+}
+
+function changeVolume(){
+    musica.volume = volumeInput.value / 100
+    volumeInput.style.backgroundSize = `${volumeInput.value}% 100%`
+}
+
+function changeCurrentTime(){
+    clearInterval(update)
+    console.log(progressInput.value)
+    musica.currentTime = progressInput.value;
+    updateInfo()
+    update = setInterval(updateInfo, 1000)
+}
+
+function updateInfo(){
+    progressInput.value = Math.floor(musica.currentTime)
+    progressInput.style.backgroundSize = Math.floor((musica.currentTime / musica.duration) * 100) + '% 100%';
     let current_Time = document.querySelector('.start');
     current_Time.textContent = secToMin(Math.floor(musica.currentTime));
+    checkIfFinished()
 }
+
+function checkIfFinished(){
+    if(musica.currentTime === musica.duration){
+        next();
+    }
+}
+
 function secToMin(secs){
     let minSpace = Math.floor(secs / 60);
     let secSpace = secs % 60;
@@ -74,4 +119,6 @@ function secToMin(secs){
 function duration(){
     duracaoMusica.textContent = secToMin(Math.floor(currentSong.duration));
 }
+
+let update = setInterval(updateInfo, 1000)
 
